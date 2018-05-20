b8a4d6d8f2de1727253c3183adf8ee4a69cf03df
AraMesropyan <aramesropyan@gmail.com>
2018-05-20 22:29:31 +0400

    Add project and some pages.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CallRequests;
+use App\MailRequest;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin-panel.home');
+    }
+
+    public function getCallRequests()
+    {
+        $calls = CallRequests::paginate(15);
+
+        return view('admin-panel.call-request', ['calls' => $calls]);
+    }
+
+    public function getMailRequests()
+    {
+        $mails = MailRequest::paginate(15);
+
+        return view('admin-panel.mail-request', ['mails' => $mails]);
+    }
+}
