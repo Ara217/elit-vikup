bafb41932514f0d4bbd452f31f7ed4ea39e0f7a6
AraMesropyan <aramesropyan@gmail.com>
2018-06-04 16:35:57 +0400

    Change logo, some stiles and meta tags and design.

--- resources/views/auth/layout/header.blade.php
+++ resources/views/auth/layout/header.blade.php
@@ -5,7 +5,7 @@
         </div>
         <ul class="nav navbar-nav navbar-logo mx-auto">
             <a href="/admin" class="mx-auto">
-                <img src="{{asset('images/ruseren-logo2.png')}}">
+                <img src="{{asset('images/ruseren-logo2.png')}}" class="logo-img">
             </a>
         </ul>
         <ul class="nav navbar-nav pull-sm-right">
